21ff9f46389a750b45a49e3371c897feda633f06
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2022-12-01 08:43:35 -0300

    caminho universal

--- JavaScript/sorte.js
+++ JavaScript/sorte.js
@@ -1,10 +1,10 @@
 function sorteioSorte(){
-    var janela = window.open("file:///C:/Users/danie/OneDrive/%C3%81rea%20de%20Trabalho/DesbravandoUniverso/JavaScript/sorteiosorte.html", "sorte", "width=700, height=700, align=center");
+    var janela = window.open("./JavaScript/sorteiosorte.html", "sorte", "width=700, height=700, align=center");
     // Ao abrir a janela, deixar o tabuleiro borrado
     janela.document.getElementById("imgjogo").style.filter = "blur(5px)";
 }
 function sorteioPergunta(){
-    var janela = window.open("file:///C:/Users/danie/OneDrive/%C3%81rea%20de%20Trabalho/DesbravandoUniverso/JavaScript/sorteiopergunta.html", "sorte", "width=700, height=700, align=center");
+    var janela = window.open("./JavaScript/sorteiopergunta.html", "sorte", "width=700, height=700, align=center");
     janela.document.getElementById("imgjogo").style.filter = "blur(5px)";
 
 }
