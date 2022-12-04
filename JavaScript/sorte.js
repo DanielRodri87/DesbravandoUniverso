38062bce0b050d89d66b3d1fbfc9c06e0e4dace6
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2022-12-04 15:39:36 -0300

    estilo

--- JavaScript/sorte.js
+++ JavaScript/sorte.js
@@ -1,10 +1,10 @@
 function sorteioSorte(){
-    var janela = window.open("./JavaScript/sorteiosorte.html", "sorte", "width=700, height=700, align=center");
+    var janela = window.open("./JavaScript/sorteiosorte.html", "sorte", "width=1366", "height=768", "align=center");
     // Ao abrir a janela, deixar o tabuleiro borrado
     janela.document.getElementById("imgjogo").style.filter = "blur(5px)";
 }
 function sorteioPergunta(){
-    var janela = window.open("./JavaScript/sorteiopergunta.html", "sorte", "width=700, height=700, align=center");
+    var janela = window.open("./JavaScript/sorteiopergunta.html", "sorte", "width=1366", "height=768", "align=center");
     janela.document.getElementById("imgjogo").style.filter = "blur(5px)";
 
 }
